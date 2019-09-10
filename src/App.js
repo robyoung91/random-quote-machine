23d454113ace12f86ca5ebf522a94b0ed25846c1
robyoung91 <robertmatthewyoung@gmail.com>
2019-09-10 21:35:44 +0100

    adding quotes and new quote generator

--- src/App.js
+++ src/App.js
@@ -1,26 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      text: '',
+      author: '',
+      jsonLength: '',
+    }
+  }
+
+  componentDidMount() {
+    fetch('https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json')
+    .then(response => response.json())
+    .then((quoteList) => {
+      this.setState({
+        quoteList: quoteList,
+        text: quoteList.quotes[0].quote,
+        author: quoteList.quotes[0].author,
+        jsonLength: quoteList.quotes.length,
+      })
+    })
+  }
+
+  handleClick = () => {
+    console.log('Howdy');
+    this.newQuote();
+  }
+
+  newQuote = () => {
+    const newIndex = Math.floor(Math.random() * Math.floor(this.state.jsonLength));
+    this.setState({
+      text: this.state.quoteList.quotes[newIndex].quote,
+      author: this.state.quoteList.quotes[newIndex].author,
+    })
+  }
+
+  render() {
+    return (
+      <div id='background'>
+          <div id='quote-box'>
+          <p id='text'>{this.state.text}</p>
+          <p id='author'>{this.state.author}</p>
+          <button id='new-quote' onClick={this.handleClick}>New Quote</button>
+          <button id='tweet-quote'></button>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default App;
