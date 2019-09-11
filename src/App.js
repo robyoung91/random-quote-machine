2f2233a5a03ff68789ec1c8f55d9f50572be701a
robyoung91 <robertmatthewyoung@gmail.com>
2019-09-11 18:35:36 +0100

    Adding background colors

--- src/App.js
+++ src/App.js
@@ -6,21 +6,22 @@ class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
+      quotes: '',
       text: '',
       author: '',
-      jsonLength: '',
+      backgroundColor: '#16a085',
     }
-  }
+  };
+
 
   componentDidMount() {
     fetch('https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json')
     .then(response => response.json())
     .then((quoteList) => {
       this.setState({
-        quoteList: quoteList,
+        quotes: quoteList.quotes,
         text: quoteList.quotes[0].quote,
         author: quoteList.quotes[0].author,
-        jsonLength: quoteList.quotes.length,
       })
     })
   }
@@ -28,28 +29,38 @@ class App extends Component {
   handleClick = () => {
     console.log('Howdy');
     this.newQuote();
+    this.changeBackground();
   }
 
   newQuote = () => {
-    const newIndex = Math.floor(Math.random() * Math.floor(this.state.jsonLength));
+    const newIndex = Math.floor(Math.random() * Math.floor(this.state.quotes.length));
     this.setState({
-      text: this.state.quoteList.quotes[newIndex].quote,
-      author: this.state.quoteList.quotes[newIndex].author,
+      text: this.state.quotes[newIndex].quote,
+      author: this.state.quotes[newIndex].author,
+    });
+  }
+
+  changeBackground = () => {
+    const colors = ['#27ae60', '#2c3e50', '#f39c12', '#e74c3c', '#9b59b6', '#FB6964', '#342224', "#472E32", "#BDBB99", "#77B1A9", "#73A857"];
+    const randomColor = Math.floor(Math.random() * Math.floor(colors.length));
+    this.setState({
+      backgroundColor: colors[randomColor],
     })
   }
 
   render() {
     return (
-      <div id='background'>
-          <div id='quote-box'>
-          <p id='text'>{this.state.text}</p>
-          <p id='author'>{this.state.author}</p>
+      <div id='background' style={{backgroundColor: this.state.backgroundColor, transition: 'all 1s',}}>
+        <div id='quote-box'>
+          <p id='text' style={{color: this.state.backgroundColor, transition: 'all 1s',}}>{this.state.text}</p>
+          <p id='author' style={{color: this.state.backgroundColor, transition: 'all 1s',}}>{this.state.author}</p>
           <button id='new-quote' onClick={this.handleClick}>New Quote</button>
           <button id='tweet-quote'></button>
         </div>
       </div>
     )
   }
+
 }
 
 export default App;
